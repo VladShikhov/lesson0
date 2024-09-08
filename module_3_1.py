1e16118b3835fcd50c4cc2cb2d519141d5984def
Vlad Shikhov <vlad1398vlad@gmail.com>
2024-09-08 15:48:50 +0400

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,29 @@
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    global count_calls
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list_to_search):
+    global count_calls
+    count_calls()
+    string = string.lower()
+    for i in range(len(list_to_search)):
+        list_to_search[i] = list_to_search[i].lower()
+    if string in list_to_search:
+        return True
+    else:
+        return False
+    
+
+calls = 0
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
