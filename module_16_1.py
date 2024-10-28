30bfd873469211dd7167cdd697a5e953c6ae70ad
Vlad Shikhov <vlad1398vlad@gmail.com>
2024-10-28 18:00:39 +0400

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -10,17 +10,17 @@ async def welkome():
 
 
 @app.get("/user/admin")
-async def welkome():
+async def admin():
     return "Вы вошли как администратор"
 
 
 @app.get("/user/{user_id}")
-async def welkome(user_id):
+async def user(user_id):
     return f"Вы вошли как пользователь №: {user_id}"
 
 
 @app.get("/user")
-async def welkome(username='name', age=0):
+async def user_info(username='name', age=0):
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
 
 
